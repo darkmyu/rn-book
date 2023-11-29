e642d569430c74322024c1d722bb27e7aed24888
Jungma <mbjun3630@naver.com>
2023-11-29 18:27:10 +0900

    항목 등록 핸들러 구현

--- TodoApp/App.tsx
+++ TodoApp/App.tsx
@@ -21,6 +21,18 @@ function App() {
     {id: 3, text: '투두리스트 만들어보기', done: false},
   ]);
 
+  const onInsert = (text: string) => {
+    const nextId =
+      todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+    const todo = {
+      id: nextId,
+      text,
+      done: false,
+    };
+
+    setTodos(todos.concat(todo));
+  };
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
@@ -29,7 +41,7 @@ function App() {
           style={styles.avoid}>
           <DateHead date={today} />
           {todos.length !== 0 ? <TodoList todos={todos} /> : <Empty />}
-          <AddTodo />
+          <AddTodo onInsert={onInsert} />
         </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
