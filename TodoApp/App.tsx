89613cfccf1fa3940c12deaa894be3d7bea033b0
Jungma <mbjun3630@naver.com>
2023-12-03 20:10:32 +0900

    AsyncStorage 저장소 사용

--- TodoApp/App.tsx
+++ TodoApp/App.tsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {KeyboardAvoidingView, Platform, StyleSheet} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import AddTodo from './components/AddTodo';
 import DateHead from './components/DateHead';
 import Empty from './components/Empty';
 import TodoList from './components/TodoList';
+import {todosStorage} from './storages/todoStorages';
 
 export interface Todo {
   id: number;
@@ -20,6 +21,14 @@ function App() {
     {id: 2, text: '리액트 네이티브 기초 공부', done: false},
     {id: 3, text: '투두리스트 만들어보기', done: false},
   ]);
+
+  useEffect(() => {
+    todosStorage.get().then(setTodos).catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    todosStorage.set(todos).catch(console.error);
+  }, [todos]);
 
   const onInsert = (text: string) => {
     const nextId =
---
+++ TodoApp/storages/todoStorages.ts
@@ -0,0 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const key = 'todos';
+
+export const todosStorage = {
+  async get() {
+    try {
+      const rawTodos = await AsyncStorage.getItem(key);
+
+      if (!rawTodos) {
+        throw new Error('No saved todos');
+      }
+
+      const savedTodos = JSON.parse(rawTodos);
+      return savedTodos;
+    } catch (e) {
+      throw new Error('Failed to load todos');
+    }
+  },
+  async set(data: any) {
+    try {
+      await AsyncStorage.setItem(key, JSON.stringify(data));
+    } catch (e) {
+      throw new Error('Failed to save todos');
+    }
+  },
+};
